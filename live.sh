dee389714c4717dab079e15ab2ae44e3e9b57d12
yasserassoud1-rgb <yasserassoud1@gmail.com>
2025-12-23 09:50:06 +0100

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# رابط m3u8
+M3U8_URL="PUT_M3U8_LINK_HERE"
+
+# Facebook Stream Key
+FB_KEY="PUT_STREAM_KEY_HERE"
+
+ffmpeg -re \
+-i "$M3U8_URL" \
+-vf "scale=1280:720" \
+-c:v libx264 -preset veryfast \
+-b:v 2000k -maxrate 2000k -bufsize 4000k \
+-c:a aac -b:a 128k -ar 44100 \
+-f flv \
+"rtmps://live-api-s.facebook.com:443/rtmp/$FB_KEY"
